98b461ab51a4d69a03416895c0cffdb082f5abc1
Jack Koontz <a66062@gmail.com>
2020-07-05 00:40:40 -0500

    first commit

---
+++ src/HelloWorldList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './HelloWorldList.css';
+
+import HelloWorld from './HelloWorld';
+import AddGreeter from "./AddGreeter";
+
+class HelloWorldList extends Component {
+    render() {
+        return (
+            <div className="HelloWorldList">
+                <AddGreeter addGreeting={this.addGreeting} />
+                {this.renderGreetings()}
+            </div>
+        );
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { greetings: ['Jack', 'Molly', 'Roscoe', 'Frida', 'Tito'] };
+        this.addGreeting = this.addGreeting.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
+    }
+
+    renderGreetings() {
+        return this.state.greetings.map(name => (
+            <HelloWorld
+                key={name}
+                name={name}
+                removeGreeting={this.removeGreeting}
+            />
+        ));
+    }
+
+    addGreeting(newName) {
+        this.setState( {greetings: [...this.state.greetings, newName]
+        });
+    }
+
+    removeGreeting(removeName) {
+        const filteredGreetings = this.state.greetings.filter(name => {
+            return name !== removeName;
+        });
+        this.setState({ greetings: filteredGreetings });
+    }
+}
+
+
+export default HelloWorldList;
